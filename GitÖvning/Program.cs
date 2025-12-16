6c03ee52268f30402fd36cfedac2d3e7e57d0398
Tony Aldervall <tony.aldervall@iths.se>
2025-12-16 15:31:11 +0100

    added hello messages

--- GitÖvning/Program.cs
+++ GitÖvning/Program.cs
@@ -5,5 +5,11 @@ internal static class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, From Tony!");
+        Console.WriteLine("Hello, From Tony!");
+        Console.WriteLine("Hello, From Tony!");
+        Console.WriteLine("Hello, From Tony!");
+        Console.WriteLine("Hello, From Tony!");
+        Console.WriteLine("Hello, From Tony!");
     }
 }
